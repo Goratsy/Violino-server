96e5287460dae733f2cf1729a3102e834f0a5156
Гор <gor@MacBook-Pro-Gor-2.local>
2025-01-31 12:43:56 +0300

    Adding authentification router and fixing requests to the db

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const JWT_SECRET = process.env.JWT_SECRET;
 const authenticateToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
     if (!authHeader) return res.sendStatus(401);
-    
+
     jwt.verify(authHeader, JWT_SECRET, (err, user) => {
         if (err) return res.sendStatus(403);
         req.user = user;
@@ -55,6 +55,10 @@ app.listen(PORT, () => {
 });
 
 // Routes
+
+app.get('/authentification_manager', authenticateToken, (req, res) => {
+    res.status(200).json({ message: 'Authentification successful' });
+})
 
 // UserPhones: Retrieve all user phones
 app.get('/user_phones', authenticateToken, async (req, res) => {
@@ -92,7 +96,7 @@ app.put('/user_phones', authenticateToken, async (req, res) => {
         for (const update of updates) {
             const { id, name, phone, date_of_send, information_about_user } = update;
             await POSTGRE_SQL_POOL.query(
-                'UPDATE user_phones SET name = $1, phone = $2, date_of_send = $3, information_about_user = $4 WHERE id = $5',
+                'UPDATE user_phones SET name = $1, phone = $2, date_of_send = $3, information_about_user = $4 WHERE user_phone_id = $5',
                 [name, phone, date_of_send, information_about_user, id]
             );
         }
@@ -106,7 +110,7 @@ app.put('/user_phones', authenticateToken, async (req, res) => {
 app.delete('/user_phones/:id', authenticateToken, async (req, res) => {
     const { id } = req.params;
     try {
-        await POSTGRE_SQL_POOL.query('DELETE FROM user_phones WHERE id = $1', [id]);
+        await POSTGRE_SQL_POOL.query('DELETE FROM user_phones WHERE user_phone_id = $1', [id]);
         res.status(204).send();
     } catch (err) {
         res.status(500).json({ error: 'Internal Server Error' });
@@ -117,23 +121,56 @@ app.delete('/user_phones/:id', authenticateToken, async (req, res) => {
 app.post('/managers/logins', async (req, res) => {
     const { login, password, date_of_login, device, ip_address } = req.body;
     try {
+        const is_in_black_list_ip = await POSTGRE_SQL_POOL.query('SELECT ip FROM black_list_ip WHERE ip = $1', [ip_address]);
+        if (is_in_black_list_ip.rows[0]) {
+            return res.status(400).json({ error: 'All attempts were wasted' });
+        }
+
         const manager = await POSTGRE_SQL_POOL.query('SELECT * FROM manager WHERE login = $1', [login]);
+
         if (manager.rows.length === 0) {
+            await POSTGRE_SQL_POOL.query('INSERT INTO black_list_ip (ip) VALUES ($1)', [ip_address]);
             return res.status(400).json({ error: 'Invalid login credentials' });
         }
+
+
+        const login_history = await POSTGRE_SQL_POOL.query('SELECT * FROM login_history WHERE ip_address = $1 AND device = $2', [ip_address, device]);
+
+        if (!login_history.rows[0]) {
+            await POSTGRE_SQL_POOL.query(
+                'INSERT INTO login_history (manager_id, date_of_login, device, ip_address) VALUES ($1, $2, $3, $4)',
+                [manager.rows[0].manager_id, date_of_login, device, ip_address]
+            );
+        } else {
+            await POSTGRE_SQL_POOL.query(
+                'UPDATE login_history SET date_of_login = $1 WHERE ip_address = $2 AND device = $3',
+                [date_of_login, ip_address, device]
+            );
+        }
+
 
         const validPassword = await bcrypt.compare(password, manager.rows[0].password);
         if (!validPassword) {
+            const failed_attempts = await POSTGRE_SQL_POOL.query('SELECT failed_login_attempts FROM login_history WHERE ip_address = $1 AND device = $2', [ip_address, device]);
+            await POSTGRE_SQL_POOL.query(
+                'UPDATE login_history SET failed_login_attempts = $1 WHERE ip_address = $2 AND device = $3',
+                [failed_attempts.rows[0].failed_login_attempts + 1, ip_address, device]
+            );
+
+            if (failed_attempts.rows[0].failed_login_attempts + 1 >= 3) {
+                await POSTGRE_SQL_POOL.query('INSERT INTO black_list_ip (ip) VALUES ($1)', [ip_address]);
+            }
+
             return res.status(400).json({ error: 'Invalid login credentials' });
+
         }
-        
-        const token = jwt.sign({ id: manager.rows[0].manager_id }, JWT_SECRET, { expiresIn: '1h' });
 
         await POSTGRE_SQL_POOL.query(
-            'INSERT INTO login_history (manager_id, date_of_login, device, ip_address, active_tokens) VALUES ($1, $2, $3, $4, $5)',
-            [manager.rows[0].manager_id, date_of_login, device, ip_address, token]
+            'UPDATE login_history SET failed_login_attempts = $1 WHERE manager_id = $2 AND device = $3',
+            [0, manager.rows[0].manager_id, device]
         );
- 
+
+        const token = jwt.sign({ id: manager.rows[0].manager_id }, JWT_SECRET, { expiresIn: '1h' });
         res.status(201).json({ token });
     } catch (err) {
         console.log(err);
@@ -144,7 +181,8 @@ app.post('/managers/logins', async (req, res) => {
 // Protected example route
 app.get('/managers', authenticateToken, async (req, res) => {
     try {
-        const result = await POSTGRE_SQL_POOL.query('SELECT * FROM manager');
+        // const result = await POSTGRE_SQL_POOL.query('SELECT manager_id, manager_name, login FROM manager');
+        const result = await POSTGRE_SQL_POOL.query('SELECT login_history.*, manager.manager_name, manager.login FROM login_history JOIN manager ON manager.manager_id = login_history.manager_id');
         res.json(result.rows);
     } catch (err) {
         res.status(500).json({ error: 'Internal Server Error' });
